e4b3c021220f30a768f918de93bc9b64bfde7c8a
rand0m <thanwisitang7910@gmail.com>
2023-07-25 11:48:08 +0700

    Dynamic image upload updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import user_icon from './user.png'
-import arrow_btn from "./arrow-triangle-button.png"
+// import logo from './images/logo.svg';
+// import user_icon from './images/user.png'
+// import arrow_btn from "./images/arrow-triangle-button.png"
+// import context from 'require-context.macro'
 import './App.css';
 import React from 'react';
+
+function retrieve_image(name) {
+  return process.env.PUBLIC_URL + `/imgs/backend-images/${name}`;
+};
 
 const DefaultPage = (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={retrieve_image("logo.svg")} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
@@ -21,7 +26,7 @@ const DefaultPage = (
       </a>
     </header>
   </div>
-)
+);
 
 function RegisterBundleButtons() {
   let registered = true;
@@ -29,21 +34,21 @@ function RegisterBundleButtons() {
   if(registered){
     return(
       <div className='user-info'>
-        <img src={user_icon} alt='user-icon'></img>
+        <img src={retrieve_image("user.png")} alt='user-icon'></img>
         <label>{userName}</label>
         <button>
-          <img src={arrow_btn}></img>
+          <img src={retrieve_image("arrow-triangle-button.png")}></img>
         </button>
       </div>
-    )
+    );
   }else{
     return(
       <div className='btn-bundle'>
         <button className='nav-btn log-in' onClick={() => window.location.replace('')}>Log in</button>
         <button className='nav-btn sign-up' onClick={() => window.location.replace('')}>Sign Up</button>
       </div>
-    )
-  }
+    );
+  };
 }
 
 const NavBar = (
@@ -60,11 +65,12 @@ const NavBar = (
       </li>
     </ul>
   </nav>
-)
+);
 
 function App() {
   return (
     <>
+      {/* <Image imgName="arrow-triangle-button.png"/> */}
       {NavBar}
     </>
   );
